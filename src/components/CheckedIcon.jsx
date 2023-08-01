b575ce1527a213bf2d7c0e278253fa13d9e56dcb
Erwan <erwanpellerinpro@gmail.com>
2023-08-01 17:23:35 +0200

    forget save for the ancien commit

--- src/components/CheckedIcon.jsx
+++ src/components/CheckedIcon.jsx
@@ -1,7 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function CheckedIcon() {
-  // This component create the arrow for the toggle on the search from the header
+  // This component create the arrow for the toggle on the search of the header
   return (
     <div>
       <FontAwesomeIcon icon="arrow-down" />
